aaf0fca96e7a211a830764855b71619668e42b40
tinect <s.koenig@tinect.de>
2024-04-10 22:59:11 +0200

    feat: add support for shopware 6.6

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,9 +1,1 @@
-import MatomoAnalyticsPlugin from "./plugin/MatomoAnalyticsPlugin";
-
-// Necessary for the webpack hot module reloading server
-if (module.hot) {
-    module.hot.accept();
-}
-
-window.PluginManager.register('MatomoAnalytics', MatomoAnalyticsPlugin);
-
+window.PluginManager.register('MatomoAnalytics', () => import('./plugin/MatomoAnalyticsPlugin'));
